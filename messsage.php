4616a5ece9e59d196c7de183baa4d1fb55ddf306
Chinedu K. Eke <ekechinedu488@gmail.com>
2021-08-20 13:06:39 +0100

    made form work

--- messsage.php
+++ messsage.php
@@ -1,55 +1,15 @@
 <?php
-  
-if($_POST) {
-    $name = "";
-    $email = "";
-    $phone = "";
-    $website = "";
-    $message = "";
-    $recipient = "ekechinedu488@gmail.com";
-    $email_body = "<div>";
-      
-    if(isset($_POST['name'])) {
-        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
-                        </div>";
-    }
- 
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['phone'])) {
-        $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$phone."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['website'])) {
-        $website = filter_var($_POST['website'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$website."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['message'])) {
-        $message = htmlspecialchars($_POST['message']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$message."</div>
-                        </div>";
-    }
-    mail($recipient, $name, $email, $message, $email_body);
-        echo "<p>Thank you for contacting me, $name. You will get a reply within 24 hours.</p>";
-   
-      
-} else {
-    echo '<p>Something went wrong</p>';
-}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$website = $_POST['website'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "ekechinedu488@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "<p>Thank you for contacting me, $name. You will get a reply within 24 hours.</p>";
+
 ?>
